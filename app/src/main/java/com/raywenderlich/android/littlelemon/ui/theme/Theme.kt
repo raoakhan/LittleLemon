a9d1d7832a9ed68b931e4ac04814ef6dcdc3207d
raoakhan <akmal_shahbaz@yahoo.com>
2023-07-09 16:30:20 +0500

    nearly complete project

--- app/src/main/java/com/raywenderlich/android/littlelemon/ui/theme/Theme.kt
+++ app/src/main/java/com/raywenderlich/android/littlelemon/ui/theme/Theme.kt
@@ -5,17 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = PrimaryGreen,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Secondary2,
+    onPrimary = Color.White
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary = PrimaryYellow,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Secondary1,
+    onPrimary = Color.Black
 
     /* Other default colors to override
     background = Color.White,
